d89a7d47ca003f575f5c4744b73124bec503081a
kevinkuja@gmail.com <kevinkuja@gmail.com@9bab5661-3045-d1d2-6091-e50cd0fb428b>
2014-12-09 22:57:49 +0000

    CHUPAME UN HUEVO

---
+++ src/random.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+  for i in $(seq 100 100 2500); do
+	#python image_random.py $i $i random_$i
+	echo $i
+	echo "vecinos"
+	python image_time.py random_$i vecino >> vecinos_random
+	echo "bilineal"
+	python image_time.py random_$i bilineal >> bilineal_random
+	echo "spline"
+	python image_time.py random_$i spline >> spline_random
+	echo "quality"
+	python image_time.py random_$i quality >> quality_random	
+  done
